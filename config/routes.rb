8ca017d8dc0a77ca66f6824025c0a95f15e8d906
Vashira <vashimgs@gmail.com>
2019-12-24 21:51:34 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'users/new'
 
@@ -6,4 +8,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
