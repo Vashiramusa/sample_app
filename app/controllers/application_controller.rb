4ba828ac4ec33d8a37b2ba2f82b66b158c145ed4
Vashira <vashimgs@gmail.com>
2020-01-07 01:14:19 +0000

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "Greetings from Vashira Musa Samaila!"
-  end
+  include SessionsHelper
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to Vashira Samaila's Sample App!"
       redirect_to @user
     else
       render 'new'
