24908fda88b82ec2b6d2a7b3c02a78fee8a1a157
Vashira <vashimgs@gmail.com>
2020-01-09 22:31:52 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,15 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
+    @users = User.where(activated: true).paginate(page: params[:page])
     @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
-
+  
   def new
     @user = User.new
   end
@@ -20,9 +22,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to Vashira Samaila's Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,14 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  
+  # Use this if developing on localhost.
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
